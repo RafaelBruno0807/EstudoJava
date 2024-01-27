21084d2a0d7c6f564d1354852f8224d0ac3e1c99
Rafael_B <rafaelbruno210@gmail.com>
2024-01-27 06:56:56 -0400

    Correção print Saldo

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -6,8 +6,10 @@ public class CaixaEletronico {
         if (valorSolicitado < saldo){
 
             saldo = saldo - valorSolicitado;
-        }
+            System.out.println("Novo saldo "+ saldo);
+        }else {
 
-        System.out.println(saldo);
+            System.out.println("Saldo insuficiente");
+        }
     }
 }
